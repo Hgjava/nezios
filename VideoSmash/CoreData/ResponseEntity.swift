c5a50ec9157351fb9916ba147649f60156d9cad6
Hgjava <haythemjava@gmail.com>
2025-10-26 05:23:44 -0700

    int

---
+++ VideoSmash/CoreData/ResponseEntity.swift
@@ -0,0 +1,14 @@
+//
+//  ResponseEntity.swift
+//  VideoSmash
+//
+//  Created by Wasiq Tayyab on 25/10/2024.
+//
+
+import Foundation
+import CoreData
+
+@objc(ResponseEntity)
+public class ResponseEntity: NSManagedObject {
+    // No additional properties or methods needed unless you want to add custom logic
+}
